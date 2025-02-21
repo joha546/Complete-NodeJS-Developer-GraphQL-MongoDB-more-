7e1cf14ac22e000d78c9a284185dfc848a46541e
Md Khaled Bin Joha <joh60483@gmail.com>
2025-02-21 17:42:01 +0600

    http server using file system

---
+++ Server/index.js
@@ -0,0 +1,31 @@
+const http = require("http");
+const fs = require("fs");
+
+const myServer = http.createServer((req, res) => {
+    const log = `${Date.now()}: ${req.url} New Req Received. \n`;
+    const header = `${Date.now()}: ${req.headers}\n`
+
+    // we're using here non-blocking request to support concurrent or parallel response.
+    fs.appendFile("log.txt", log, (err, data) => {
+        fs.appendFile("header.txt", header, (err, data)=>{
+            switch(req.url){
+                case '/':
+                    res.end("Hello From Server");
+                    break;
+                case '/about':
+                    res.end("I am Md Khaled Bin Joha");
+                    break;
+                case '/contact-us':
+                    res.end("Contact No : 01828262134")
+                    break;
+                default:
+                    res.end("404 Not Found.");
+                    break;
+            }
+            
+        });
+    });
+    // console.log(req.headers);
+});
+
+myServer.listen(8000, () => console.log("Server Started..."));
